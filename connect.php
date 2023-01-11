2087d12769b3f88cd2e3c291a71d62b1a8d710bd
Navin Pandey <45593081+imnavin007@users.noreply.github.com>
2023-01-11 19:11:53 +0545

    f

---
+++ connect.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] =='POST' && isset($_POST['submit'])){
+    $conn= mysqli_connect('localhost','root','',jana_db) or die("Connection Failed": .mysql_connect_error());
+    if (isset($_POST['name'])&& isset($_POST['st'])&& isset($_POST['medname'])&& isset($_POST['med'])&& isset($_POST['phone'])&& isset($_POST['message']))
+    {
+    $name=$_POST['name'];
+    $service=$_POST['st'];
+    $medname=$_POST['medname'];
+    $med=$_POST['med'];
+    $phone=$_POST['phone'];
+    $message=$_POST['message']; 
+    $sql = "INSERT INTO 'user'('name','service','medname','med','phone','message')";
+    $query = mysqli_query($conn,$sql);
+    if($query)
+    {
+        echo'Entry Succesfull';
+    }
+    else{
+        echo 'Error Occured';
+    }
+    }
+}
+?>

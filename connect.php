45a12d8337a2e914bcc50afc5a8011f9afc9ae37
Navin Pandey <45593081+imnavin007@users.noreply.github.com>
2023-01-11 19:12:47 +0545

    d

--- connect.php
+++ connect.php
@@ -1,22 +1,23 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] =='POST' && isset($_POST['submit'])){
-    $conn= mysqli_connect('localhost','root','',jana_db) or die("Connection Failed": .mysql_connect_error());
-    if (isset($_POST['name'])&& isset($_POST['st'])&& isset($_POST['medname'])&& isset($_POST['med'])&& isset($_POST['phone'])&& isset($_POST['message']))
+if ($_SERVER["REQUEST_METHOD"] =='POST' && isset($_POST['submit'])){
+    $conn=mysqli_connect('localhost','root','','jana_db') or die("Connection Failed:".mysqli_connect_error());
+    if (isset($_POST['name'])&& isset($_POST['service'])&& isset($_POST['medname'])&& isset($_POST['med'])&& isset($_POST['phone'])&& isset($_POST['message']))
     {
     $name=$_POST['name'];
-    $service=$_POST['st'];
+    $service=$_POST['service'];
     $medname=$_POST['medname'];
     $med=$_POST['med'];
     $phone=$_POST['phone'];
     $message=$_POST['message']; 
-    $sql = "INSERT INTO 'user'('name','service','medname','med','phone','message')";
+
+    $sql = "INSERT INTO `user`(`name`, `service`, `medname`, `med`, `phone`, `message`) VALUES ('$name','$service','$medname','$med','$phone','$message')";
     $query = mysqli_query($conn,$sql);
     if($query)
     {
-        echo'Entry Succesfull';
+        echo 'Entry Succesfull';
     }
     else{
-        echo 'Error Occured';
+        echo  'Error Occured';
     }
     }
 }

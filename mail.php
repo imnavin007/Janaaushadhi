407695268cbf53eba9091772c635599d6d7af2fd
Navin Pandey <45593081+imnavin007@users.noreply.github.com>
2023-01-12 19:16:52 +0545

    alha

--- mail.php
+++ mail.php
@@ -9,7 +9,7 @@ $to = "janaaushadhipharmacy@gmail.com";
 $subject = "Message From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email ."\r\n Contact =" . $phone. "\r\n Subject =" . $subject."\r\n Message =" . $message;
 $headers = "From: message@janaaushadhi.com.np";
-if($email!=NULL){
+if($name!=NULL){
     mail($to,$subject,$txt,$headers);
 }
 //redirect

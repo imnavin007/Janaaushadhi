989e0b0ad26198303d0a47da5a5ad4a87aca892c
Navin Pandey <45593081+imnavin007@users.noreply.github.com>
2023-01-12 19:12:57 +0545

    again

--- mail.php
+++ mail.php
@@ -2,14 +2,16 @@
 //get data from form  
 $name = $_POST['name'];
 $email= $_POST['email'];
+$phone= $_POST['phone'];
+$subject = $_POST['subject']; 
 $message= $_POST['message'];
-$to = "youremail@mail.com";
-$subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com";
+$to = "janaaushadhipharmacy@gmail.com";
+$subject = "Message From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email ."\r\n Contact =" . $phone. "\r\n Subject =" . $subject."\r\n Message =" . $message;
+$headers = "From: message@janaaushadhi.com.np";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
 //redirect
-header("Location:thankyou.html");
+header("Location:index.html");
 ?>
